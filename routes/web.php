e189138046e20695fddfc1e2ca717c2a20a49673
AmalFauzan <165254792+AmalFauzan@users.noreply.github.com>
2024-05-28 22:42:26 +0700

    "third commit"

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,14 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
-Route::get('/login', function () {
-    return view('login');
-});
-Route::get('/', function () {
-    return view('menu');
-});
+Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('dashboard', function () {
+    if (!Session::get('authenticated')) {
+        return redirect('login');
+    }
+    return view('dashboard');
+})->name('dashboard');
